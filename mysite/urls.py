6755cc6fdbd85ec609f3ea928b59e6d4d510cb7d
Your Name <you@example.com>
2026-03-04 19:27:43 -0500

    wagtail added

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
-
 from django.contrib.auth import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -26,7 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('cms/', include(wagtailadmin_urls)),
     path('', include(wagtail_urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
